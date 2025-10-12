e86923fe2a13f8ff13e483b316078d826fd4e007
Dolly Suman <dolly.suman.023@gmail.com>
2025-10-12 19:56:06 +0530

    new feature added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,29 @@ import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [todo, setTodo] = useState("")
+  const [todos, setTodos] = useState([])
+
+  const handleEdit = ()=>{
+    
+  }
+
+
+  const handleDelete = ()=>{
+
+  }
+
+
+  const handleAdd = ()=>{
+    setTodos([...todos,{todo,iscompleted:false}])
+    setTodo("")
+    console.log(todos)
+  }
+
+  const handleChange = (e)=>{
+    
+    setTodo(e.target.value)
+  }
 
   return (
     <>
@@ -13,15 +36,17 @@ function App() {
       <div className="container mx-auto bg-violet-100 rounded-xl p-3 min-h-[80vh]"> 
         <div className="addtodo my-5">
           <h2 className='text-lg font-bold'>Add a ToDo</h2>
-          <input className='bg-white w-1/2' type="text" placeholder='Add Your Tasks here' />
-          <button className=' text-sm font-bold bg-violet-800 hover:bg-violet-950 text-white rounded-md p-2 py-1 m-4'>Add</button>
+          <input onChange={handleChange} value={todo} className='bg-white w-1/2' type="text" placeholder='Add Your Tasks here' />
+          <button onClick={handleAdd} className=' text-sm font-bold bg-violet-800 hover:bg-violet-950 text-white rounded-md p-2 py-1 m-4'>Add</button>
           </div> 
           <h2 className='text-lg font-bold'>Your TO DO</h2>
           <div className="todos">
-            <div className="todo flex justify-between">
-              <div className="text">
-              Lorem ipsum dolor, sit amet  assumenda fugit dolores!
-              </div>
+            {todos.map(item=> {
+
+            
+           return <div key={todo} className="todo w-1/4 flex justify-between my-3">
+            <input type="checkbox" value={todo.iscompleted} name="" id="" />
+              <div className={item.iscompleted?"line-through" : ""}>{item.todo}</div>
               <div className="buttons">
                 <button onClick={handleEdit} className=' text-sm font-bold bg-violet-800 hover:bg-violet-950 text-white rounded-md p-2 py-1 m-1'>Edit</button>
                 <button onClick={handleDelete} className=' text-sm font-bold bg-violet-800 hover:bg-violet-950 text-white rounded-md p-2 py-1 m-1'>Delete</button>
@@ -29,6 +54,7 @@ function App() {
               </div>
 
             </div>
+              })}
           </div>
         
       </div>
